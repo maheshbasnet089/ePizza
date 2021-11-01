b4150728677ce20ab38eecae83c3427759b98460
manishbasnet089 <basnetmanish089@gmail.com>
2021-11-01 20:25:22 +0545

    index Controller added !

---
+++ app/http/controllers/homeController.js
@@ -0,0 +1,16 @@
+const Menu = require("../../models/menu");
+function homeController() {
+  return {
+    async index(req, res) {
+      try {
+        const pizzas = await Menu.find();
+        res.render("home", { pizzas: pizzas });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  };
+}
+
+module.exports = homeController;
+s;
--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const path = require("path");
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 
+// Database connection
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
+const connection = mongoose.connection;
+connection
+  .once("open", () => {
+    console.log("Database connected...");
+  })
+  .catch((err) => {
+    console.log("Connection failed...");
+  });
+
 //view engine setup
 // app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
