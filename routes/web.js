93d70560d8cadb1fe3eeffbcff68343057f78ddb
manishbasnet089 <basnetmanish089@gmail.com>
2021-11-02 08:49:01 +0545

    auth template start

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,9 @@
 const homeController = require("../app/http/controllers/homeController");
+const authController = require('../app/http/controllers/authController')
 function initRoute(app) {
   app.get("/", homeController().index);
+  app.get('/login', authController().login)
+  app.get('/register', authController().register)
 }
 
 module.exports = initRoute;
