2ea626e47789f79ba19e0f13519423c5aa185571
manishbasnet089 <basnetmanish089@gmail.com>
2021-11-03 09:19:49 +0545

    package-lock delete

--- routes/web.js
+++ routes/web.js
@@ -1,9 +1,11 @@
 const homeController = require("../app/http/controllers/homeController");
-const authController = require('../app/http/controllers/authController')
+const authController = require("../app/http/controllers/authController");
+const cartController = require("../app/http/controllers/customers/cartController");
 function initRoute(app) {
   app.get("/", homeController().index);
-  app.get('/login', authController().login)
-  app.get('/register', authController().register)
+  app.get("/login", authController().login);
+  app.get("/register", authController().register);
+  app.get("/cart", cartController().index);
 }
 
 module.exports = initRoute;
